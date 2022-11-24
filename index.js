039b26981b621d96c53f8425612c720f1e667adb
Michaela Brown <m_brownbrown@yahoo.com>
2022-11-23 18:23:49 -0800

    HTML textcontent removal changes

--- index.js
+++ index.js
@@ -1,35 +1,6 @@
-// let oneActivity = document.getElementById('top-activity-title')
-// let oneDesc = document.getElementById('top-activity-description')
-// let randomizer = document.getElementById('random')
-
-
-// randomizer.addEventListener('click', (event) => {
-//     event.preventDefault();
-
-//     fetch("https://www.boredapi.com/api/activity")
-//     .then(function (res) {
-//         return res.json();
-//     })
-//     .then(function(data) {
-//         let dataType;
-//         if (data.type === 'diy') {
-//             dataType = data.type.toUpperCase();
-//         } else if (data.type !== 'diy') {
-//             let dataTypeUpper = data.type[0]
-//             let dataTypeLower = data.type.slice(1)
-//             dataTypeUpper = dataTypeUpper.toUpperCase()
-//             dataType = dataTypeUpper + dataTypeLower;
-//         }
-//         let dataPrice = data.price * 10;
-//         oneActivity.innerText = `${data.activity}`;
-//         oneDesc.innerHTML = `Type: ${dataType} <br><br> Participants: ${data.participants} <br><br> Price: $${dataPrice}`;
-//     })
-// })
-
 let oneActivity = document.getElementById('top-activity-title')
-let oneDesc = document.getElementById('top-activity-description')
 let randomizer = document.getElementById('random')
-
+let subActivities = document.getElementsByClassName('sub-act')
 
 randomizer.addEventListener('click', (event) => {
     event.preventDefault();

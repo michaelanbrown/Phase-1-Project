b161502007b9c70afea9885a68e67d8b702a9c74
Michaela Brown <m_brownbrown@yahoo.com>
2022-11-23 17:42:30 -0800

    Added first API and randomier button

---
+++ index.js
@@ -0,0 +1,20 @@
+let oneDesc = document.getElementById('top-activity-description')
+
+fetch("https://www.boredapi.com/api/activity")
+.then(function (res) {
+    return res.json();
+})
+.then(function(data) {
+    console.log(data)
+    let dataType;
+    if (data.type === 'diy') {
+        dataType = data.type.toUpperCase();
+    } else if (data.type !== 'diy') {
+        let dataTypeUpper = data.type[0]
+        let dataTypeLower = data.type.slice(1)
+        dataTypeUpper = dataTypeUpper.toUpperCase()
+        dataType = dataTypeUpper + dataTypeLower;
+    }
+    const dataPrice = data.price * 10;
+    oneDesc.innerHTML = `Type: ${dataType} <br><br> Participants: ${data.participants} <br><br> Price: $${dataPrice}`
+})

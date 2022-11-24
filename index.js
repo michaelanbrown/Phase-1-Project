57d807c1809d668349f2b358a2aca66c8c51376e
Michaela Brown <m_brownbrown@yahoo.com>
2022-11-23 18:10:57 -0800

    API title and description working

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
+// let oneActivity = document.getElementById('top-activity-title')
+// let oneDesc = document.getElementById('top-activity-description')
+// let randomizer = document.getElementById('random')
+
+
+// randomizer.addEventListener('click', (event) => {
+//     event.preventDefault();
+
+//     fetch("https://www.boredapi.com/api/activity")
+//     .then(function (res) {
+//         return res.json();
+//     })
+//     .then(function(data) {
+//         let dataType;
+//         if (data.type === 'diy') {
+//             dataType = data.type.toUpperCase();
+//         } else if (data.type !== 'diy') {
+//             let dataTypeUpper = data.type[0]
+//             let dataTypeLower = data.type.slice(1)
+//             dataTypeUpper = dataTypeUpper.toUpperCase()
+//             dataType = dataTypeUpper + dataTypeLower;
+//         }
+//         let dataPrice = data.price * 10;
+//         oneActivity.innerText = `${data.activity}`;
+//         oneDesc.innerHTML = `Type: ${dataType} <br><br> Participants: ${data.participants} <br><br> Price: $${dataPrice}`;
+//     })
+// })
+
+let oneActivity = document.getElementById('top-activity-title')
 let oneDesc = document.getElementById('top-activity-description')
 let randomizer = document.getElementById('random')
+
 
 randomizer.addEventListener('click', (event) => {
     event.preventDefault();
@@ -18,7 +48,11 @@ randomizer.addEventListener('click', (event) => {
             dataTypeUpper = dataTypeUpper.toUpperCase()
             dataType = dataTypeUpper + dataTypeLower;
         }
-        const dataPrice = data.price * 10;
-        oneDesc.innerHTML = `Type: ${dataType} <br><br> Participants: ${data.participants} <br><br> Price: $${dataPrice}`
+        let dataPrice = data.price * 10;
+        oneActivity.innerHTML = `<br>${data.activity}`;
+        let oneDesc = document.createElement('p');
+        oneDesc.setAttribute('id', 'top-activity-description')
+        oneDesc.innerHTML = `Type: ${dataType} <br><br> Participants: ${data.participants} <br><br> Price: $${dataPrice}`;
+        oneActivity.appendChild(oneDesc);
     })
 })

48cd5f9f42e2a39ca3672793ddabd09684155997
Michaela Brown <m_brownbrown@yahoo.com>
2022-11-23 17:48:07 -0800

    Added a click event listener to my randomizer button

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 let oneDesc = document.getElementById('top-activity-description')
+let randomizer = document.getElementById('random')
 
-fetch("https://www.boredapi.com/api/activity")
-.then(function (res) {
-    return res.json();
-})
-.then(function(data) {
-    console.log(data)
-    let dataType;
-    if (data.type === 'diy') {
-        dataType = data.type.toUpperCase();
-    } else if (data.type !== 'diy') {
-        let dataTypeUpper = data.type[0]
-        let dataTypeLower = data.type.slice(1)
-        dataTypeUpper = dataTypeUpper.toUpperCase()
-        dataType = dataTypeUpper + dataTypeLower;
-    }
-    const dataPrice = data.price * 10;
-    oneDesc.innerHTML = `Type: ${dataType} <br><br> Participants: ${data.participants} <br><br> Price: $${dataPrice}`
+randomizer.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    fetch("https://www.boredapi.com/api/activity")
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function(data) {
+        let dataType;
+        if (data.type === 'diy') {
+            dataType = data.type.toUpperCase();
+        } else if (data.type !== 'diy') {
+            let dataTypeUpper = data.type[0]
+            let dataTypeLower = data.type.slice(1)
+            dataTypeUpper = dataTypeUpper.toUpperCase()
+            dataType = dataTypeUpper + dataTypeLower;
+        }
+        const dataPrice = data.price * 10;
+        oneDesc.innerHTML = `Type: ${dataType} <br><br> Participants: ${data.participants} <br><br> Price: $${dataPrice}`
+    })
 })

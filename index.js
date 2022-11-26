7793a85c5b6b63494b0622d2933803448ca4a37b
Michaela Brown <m_brownbrown@yahoo.com>
2022-11-25 18:24:51 -0800

    5 fetches for 5 pieces of data

--- index.js
+++ index.js
@@ -2,28 +2,82 @@ let oneActivity = document.getElementById('top-activity-title')
 let randomizer = document.getElementById('random')
 let subActivities = document.getElementsByClassName('sub-act')
 
-randomizer.addEventListener('click', (event) => {
-    event.preventDefault();
+//     randomizer.addEventListener('click', (event) => {
+//         event.preventDefault();
+    
+//         fetch("https://www.boredapi.com/api/activity")
+//         .then(function (res) {
+//             return res.json();
+//         })
+//         .then(function(data) {
+//             let dataType;
+//             if (data.type === 'diy') {
+//                 dataType = data.type.toUpperCase();
+//             } else if (data.type !== 'diy') {
+//                 let dataTypeUpper = data.type[0]
+//                 let dataTypeLower = data.type.slice(1)
+//                 dataTypeUpper = dataTypeUpper.toUpperCase()
+//                 dataType = dataTypeUpper + dataTypeLower;
+//             }
+//             let dataPrice = data.price * 10;
+//             oneActivity.innerHTML = `<br>${data.activity}`;
+//             let oneDesc = document.createElement('p');
+//             oneDesc.setAttribute('id', 'top-activity-description')
+//             oneDesc.innerHTML = `Type: ${dataType} <br><br> Participants: ${data.participants} <br><br> Price: $${dataPrice}`;
+//             oneActivity.appendChild(oneDesc);
+//         })
+//     })
 
+randomizer.addEventListener('click', (event) => {
+    let dataArray = [];
+    fetch("https://www.boredapi.com/api/activity")
+.then(function (res) {
+    return res.json();
+})
+.then(function(data) {
+    dataArray.push(data)
+})
+.then(function(data) {
     fetch("https://www.boredapi.com/api/activity")
     .then(function (res) {
         return res.json();
     })
     .then(function(data) {
-        let dataType;
-        if (data.type === 'diy') {
-            dataType = data.type.toUpperCase();
-        } else if (data.type !== 'diy') {
-            let dataTypeUpper = data.type[0]
-            let dataTypeLower = data.type.slice(1)
-            dataTypeUpper = dataTypeUpper.toUpperCase()
-            dataType = dataTypeUpper + dataTypeLower;
-        }
-        let dataPrice = data.price * 10;
-        oneActivity.innerHTML = `<br>${data.activity}`;
-        let oneDesc = document.createElement('p');
-        oneDesc.setAttribute('id', 'top-activity-description')
-        oneDesc.innerHTML = `Type: ${dataType} <br><br> Participants: ${data.participants} <br><br> Price: $${dataPrice}`;
-        oneActivity.appendChild(oneDesc);
+        dataArray.push(data)
+    })
+    .then(function(data) {
+        fetch("https://www.boredapi.com/api/activity")
+        .then(function (res) {
+        return res.json();
+    })
+    .then(function(data) {
+        dataArray.push(data)
+        })
+        .then(function(data) {
+            fetch("https://www.boredapi.com/api/activity")
+            .then(function (res) {
+            return res.json();
+            })
+            .then(function(data) {
+            dataArray.push(data)
+            }) 
+            .then(function(data) {
+                fetch("https://www.boredapi.com/api/activity")
+                .then(function (res) {
+                return res.json();
+                })
+                .then(function(data) {
+                dataArray.push(data)
+                    }) 
+                .then(function(data) {
+                    oneActivity.innerHTML = `<br>${dataArray[0].activity}`;
+                    let oneDesc = document.createElement('p');
+                    oneDesc.setAttribute('id', 'top-activity-description')
+                    oneDesc.innerHTML = `Type: ${dataArray[0].type} <br><br> Participants: ${dataArray[0].participants} <br><br> Price: $${dataArray[0].price}`;
+                    oneActivity.appendChild(oneDesc);
+                    })    
+                })
+            }) 
+        })   
     })
 })

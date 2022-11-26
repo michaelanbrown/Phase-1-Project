b27104e6dbfdd5d1d33c849b405c029bafdd254d
Michaela Brown <m_brownbrown@yahoo.com>
2022-11-25 18:51:04 -0800

    mapped array to adjust price

--- index.js
+++ index.js
@@ -6,32 +6,6 @@ let fiveActivity = document.getElementById('five-activity')
 let randomizer = document.getElementById('random')
 let subActivities = document.getElementsByClassName('sub-act')
 
-//     randomizer.addEventListener('click', (event) => {
-//         event.preventDefault();
-    
-//         fetch("https://www.boredapi.com/api/activity")
-//         .then(function (res) {
-//             return res.json();
-//         })
-//         .then(function(data) {
-//             let dataType;
-//             if (data.type === 'diy') {
-//                 dataType = data.type.toUpperCase();
-//             } else if (data.type !== 'diy') {
-//                 let dataTypeUpper = data.type[0]
-//                 let dataTypeLower = data.type.slice(1)
-//                 dataTypeUpper = dataTypeUpper.toUpperCase()
-//                 dataType = dataTypeUpper + dataTypeLower;
-//             }
-//             let dataPrice = data.price * 10;
-//             oneActivity.innerHTML = `<br>${data.activity}`;
-//             let oneDesc = document.createElement('p');
-//             oneDesc.setAttribute('id', 'top-activity-description')
-//             oneDesc.innerHTML = `Type: ${dataType} <br><br> Participants: ${data.participants} <br><br> Price: $${dataPrice}`;
-//             oneActivity.appendChild(oneDesc);
-//         })
-//     })
-
 randomizer.addEventListener('click', (event) => {
     let dataArray = [];
     fetch("https://www.boredapi.com/api/activity")
@@ -74,6 +48,10 @@ randomizer.addEventListener('click', (event) => {
                 dataArray.push(data)
                     }) 
                 .then(function(data) {
+                    dataArray = dataArray.map(function(element) {
+                        return {activity: element.activity, type: element.type, participants: element.participants, price: element.price*10}
+                    })
+                    console.log(dataArray)
                     oneActivity.innerHTML = `<br>${dataArray[0].activity}`;
                     let oneDesc = document.createElement('p');
                     oneDesc.setAttribute('id', 'top-activity-description')
